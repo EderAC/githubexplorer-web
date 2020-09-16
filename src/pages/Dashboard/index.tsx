a34653c0e8dbbec8d0c3d9fa71179d65046fd831
EderAC <eder.almeida40@gmail.com>
2020-09-16 16:46:46 -0300

    create a conection between the application and the API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,59 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
+// cria a tipagem somente do que vai utilizar
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 // FC functionComponents
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    // Adição de um repositório
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/42493587?s=400&u=087883c03b466ad7860412f12a3e2d6233ab4ef1&v=4"
-            alt="Eder Chiquito"
-          />
-          <div>
-            <strong>EderAC/LGPD_QuizFE Private</strong>
-            <p>No description, website, or topics provided.</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/42493587?s=400&u=087883c03b466ad7860412f12a3e2d6233ab4ef1&v=4"
-            alt="Eder Chiquito"
-          />
-          <div>
-            <strong>EderAC/LGPD_QuizFE Private</strong>
-            <p>No description, website, or topics provided.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/42493587?s=400&u=087883c03b466ad7860412f12a3e2d6233ab4ef1&v=4"
-            alt="Eder Chiquito"
-          />
-          <div>
-            <strong>EderAC/LGPD_QuizFE Private</strong>
-            <p>No description, website, or topics provided.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
